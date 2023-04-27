770c77bec01e40aee5bdcaafec664d83ee20576c
Mahir Labib Chowdhury <mahir.labib@pathao.com>
2023-04-28 05:35:14 +0600

    chore: generated cobra project

---
+++ cmd/root.go
@@ -0,0 +1,74 @@
+/*
+Copyright © 2023 Mahir Labib Chowdhury dev.mahirchy@gmail.com
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "taskchecklist",
+	Short: "Server application for a checklist application",
+	Long: `Server application for a checklist application.
+
+The primary goal of this application is to have a checklist
+for repeated tasks. It is to help with not missing any
+important steps of the tasks.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ./config/.taskchecklist.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find configDir directory.
+		configDir := "config"
+
+		// Search config in configDir directory with name ".taskchecklist" (without extension).
+		viper.AddConfigPath(configDir)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".taskchecklist")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		_, _ = fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 Mahir Labib Chowdhury dev.mahirchy@gmail.com
+*/
+package main
+
+import "github.com/m4hi2/taskchecklist/cmd"
+
+func main() {
+	cmd.Execute()
+}
